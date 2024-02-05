dc75ab8fdba39f8714f642b1cfeda85bdd36013b
victorlopescc <victorlopes.cc@gmail.com>
2024-02-05 14:01:06 -0300

    feat: Auth hook

---
+++ src/hooks/auth.jsx
@@ -0,0 +1,77 @@
+import { createContext, useContext, useState, useEffect } from 'react'
+import jwtDecode from 'jwt-decode'
+
+import { api } from '../services/api'
+
+export const AuthContext = createContext({})
+
+function AuthProvider({ children }) {
+    const [data, setData] = useState({})
+
+    function signOut() {
+        localStorage.removeItem('@foodexplorer:user')
+        localStorage.removeItem('@foodexplorer:token')
+
+        setData({})
+    }
+
+    function isUserAuthenticated() {
+        const user = localStorage.getItem('@foodexplorer:user')
+
+        if (!user) return false
+
+        const token = localStorage.getItem('@foodexplorer:token')
+        const tokenExpiration = jwtDecode(token).exp
+        const currentTime = Math.floor(Date.now() / 1000)
+
+        if (currentTime > tokenExpiration) return false
+
+        return true
+    }
+
+    async function signIn({ email, password }) {
+        try {
+            const response = await api.post('/sessions', { email, password })
+            const { user, token } = response.data
+
+            localStorage.setItem('@foodexplorer:user', JSON.stringify(user))
+            localStorage.setItem('@foodexplorer:token', token)
+
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+            setData({ user, token })
+        } catch (error) {
+            if (error.response) alert(error.response.data.message)
+            else alert('Erro ao realizar login')
+        }
+    }
+
+    useEffect(() => {
+        const user = localStorage.getItem('@foodexplorer:user')
+        const token = localStorage.getItem('@foodexplorer:token')
+
+        if (token && user) {
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+            setData({ user: JSON.parse(user), token })
+        }
+    }, [])
+
+    return (
+        <AuthContext.Provider
+            value={{
+                signIn,
+                signOut,
+                isUserAuthenticated,
+                user: data.user
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+function useAuth() {
+    const context = useContext(AuthContext)
+    return context
+}
+
+export { AuthProvider, useAuth }
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
-// import { AuthProvider } from './styles/theme'
+import { AuthProvider } from './hooks/auth'
 // import { Routes } from './routes'
 
 
@@ -13,9 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      {/* <AuthProvider> */}
-      {/* <Routes /> */}
-      {/* </AuthProvider> */}
+      <AuthProvider>
+        {/* <Routes /> */}
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
