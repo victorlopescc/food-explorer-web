8a69d5d578eb2c4cf09ce2eec674efb6452dfc7e
victorlopescc <victorlopes.cc@gmail.com>
2024-02-05 15:04:54 -0300

    fix: JWT decode version

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from 'react'
-import jwtDecode from 'jwt-decode'
+import jwtDecode from "jwt-decode";
 
 import { api } from '../services/api'
 
